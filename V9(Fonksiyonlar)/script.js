b788de6e0e304f69245be6b12de2dee876dd3560
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-10-01 16:34:00 +0300

    Video 8 Bitti

---
+++ V9(Fonksiyonlar)/script.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp(
+    {
+        data(){
+            return {
+                firstGoal:'Html',
+                website:'https://www.google.com',
+            }
+        }
+    }
+);
+
+app.mount('#frontend');
