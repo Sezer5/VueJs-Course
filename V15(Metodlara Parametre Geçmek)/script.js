334949dbb58dfe294a236bd0a94a41e439aca100
Sezer Ünalmış <unalmissezer@gmail.com>
2025-10-05 16:54:27 +0300

    Input Event'a Kadar

---
+++ V15(Metodlara Parametre Geçmek)/script.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp(
+    {
+        data(){
+            return {
+                counter:0,
+            }
+        },
+        methods:{
+            add(number){
+                this.counter=this.counter+number;
+            },
+            reduce(number){
+                this.counter=this.counter-number;
+            },
+        }
+    }
+);
+
+app.mount('#frontend');
