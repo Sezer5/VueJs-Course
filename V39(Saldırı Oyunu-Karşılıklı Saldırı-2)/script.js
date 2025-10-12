ca192b58970883b42adbc82a1023bd13909fc9be
Sezer Ünalmış <unalmissezer@gmail.com>
2025-10-12 22:38:33 +0300

    Karşılıklı Saldırı Başlangıcı

---
+++ V39(Saldırı Oyunu-Karşılıklı Saldırı-2)/script.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp(
+    {
+        data(){
+            return {
+                
+                
+            }
+        },
+        watch:{
+            
+            
+        },
+        computed:{
+           
+        },
+        methods:{
+         
+        }
+    }
+);
+
+app.mount('#frontend');
