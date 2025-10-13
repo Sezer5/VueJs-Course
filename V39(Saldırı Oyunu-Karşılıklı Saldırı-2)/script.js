02dd733bfad497b4f0994e76c3481b147184cf42
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-10-13 17:27:54 +0300

    V39 Sonu

--- V39(Saldırı Oyunu-Karşılıklı Saldırı-2)/script.js
+++ V39(Saldırı Oyunu-Karşılıklı Saldırı-2)/script.js
@@ -1,8 +1,15 @@
+function getRandomValue(min,max){
+    return Math.floor(Math.random()*(max-min))+min,
+}
+
+
 const app = Vue.createApp(
     {
         data(){
             return {
-                
+                winner:null,
+                myHealth:100,
+                computerHealth:100
                 
             }
         },
@@ -14,7 +21,15 @@ const app = Vue.createApp(
            
         },
         methods:{
-         
+            attackToComputer(){
+                const attackValue=getRandomValue(7,15);
+                this.computerHealth=this.computerHealth-attackValue;
+                this.attackMe()
+            },
+            attackMe(){
+                const attackValue=getRandomValue(10,20);
+                this.myHealth=this.myHealth-attackValue;
+            }
         }
     }
 );
