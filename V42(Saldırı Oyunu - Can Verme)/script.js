aef8f45e4a715f1a045ace672b8fbc10f91ab047
Sezer Ünalmış <unalmissezer@gmail.com>
2025-10-14 22:29:50 +0300

    Saldırı Oyunu Bitişi

---
+++ V42(Saldırı Oyunu - Can Verme)/script.js
@@ -0,0 +1,67 @@
+function getRandomValue(min,max){
+    return Math.floor(Math.random()*(max-min))+min;
+};
+
+
+const app = Vue.createApp(
+    {
+        data(){
+            return {
+                winner:null,
+                myHealth:100,
+                computerHealth:100,
+                
+            }
+        },
+        watch:{
+            
+            
+        },
+        computed:{
+           computerBarStyles(){
+                if(this.computerHealth<0){
+                    return {width:'0%'};
+                }else{
+                    return {width:this.computerHealth + '%'};
+                }
+                
+           },
+           myBarStyles(){
+                if(this.myHealth<0){
+                    return {width:'0%'};
+                }else{
+                    return {width:this.myHealth + '%'};
+                }
+                
+           },
+        },
+        methods:{
+            attackToComputer(){
+                const attackValue=getRandomValue(7,15);
+                this.computerHealth=this.computerHealth-attackValue;
+                this.attackMe()
+            },
+            attackMe(){
+                const attackValue=getRandomValue(10,20);
+                this.myHealth=this.myHealth-attackValue;
+            },
+            specialAttack(){
+                const attackValue=getRandomValue(15,30);
+                this.computerHealth=this.computerHealth-attackValue;
+                this.attackMe()
+            },
+            healMe(){
+                const healValue=getRandomValue(15,20);
+                if(this.myHealth + healValue > 100){
+                    this.myHealth=100;
+                }else{
+                    this.myHealth+=healValue;
+                }
+                
+                this.attackMe()
+            }
+        }
+    }
+);
+
+app.mount('#frontend');
---
+++ V43(Saldırı Oyunu - Süper Saldırı Modunun Açılması)/script.js
@@ -0,0 +1,74 @@
+function getRandomValue(min,max){
+    return Math.floor(Math.random()*(max-min))+min;
+};
+
+
+const app = Vue.createApp(
+    {
+        data(){
+            return {
+                winner:null,
+                myHealth:100,
+                computerHealth:100,
+                currentRound:0,
+                
+            }
+        },
+        watch:{
+            
+            
+        },
+        computed:{
+           computerBarStyles(){
+                if(this.computerHealth<0){
+                    return {width:'0%'};
+                }else{
+                    return {width:this.computerHealth + '%'};
+                }
+                
+           },
+           myBarStyles(){
+                if(this.myHealth<0){
+                    return {width:'0%'};
+                }else{
+                    return {width:this.myHealth + '%'};
+                }
+                
+           },
+           possibleSpecialAttack(){
+                return this.currentRound % 4 !== 0;
+           }
+        },
+        methods:{
+            attackToComputer(){
+                this.currentRound+=1;
+                const attackValue=getRandomValue(7,15);
+                this.computerHealth=this.computerHealth-attackValue;
+                this.attackMe()
+            },
+            attackMe(){
+                const attackValue=getRandomValue(10,20);
+                this.myHealth=this.myHealth-attackValue;
+            },
+            specialAttack(){
+                this.currentRound+=1;
+                const attackValue=getRandomValue(15,30);
+                this.computerHealth=this.computerHealth-attackValue;
+                this.attackMe()
+            },
+            healMe(){
+                const healValue=getRandomValue(15,20);
+                this.currentRound+=1;
+                if(this.myHealth + healValue > 100){
+                    this.myHealth=100;
+                }else{
+                    this.myHealth+=healValue;
+                }
+                
+                this.attackMe()
+            }
+        }
+    }
+);
+
+app.mount('#frontend');
